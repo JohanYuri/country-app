0683d1395b1a11da543093e08ea2de0bdfbb0395
JohanYuri <johanyuri24@gmail.com>
2025-10-02 02:01:21 -0600

    Ruta de más información hecha

--- src/app/country/components/country-list/country-list.component.ts
+++ src/app/country/components/country-list/country-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Importa CommonModule
+import { CommonModule, DecimalPipe, NgFor, NgIf } from '@angular/common'; // Importa CommonModule
 import { Country } from '../../interfaces/country.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'country-list',
   standalone: true,
-  imports: [CommonModule], // Agrega CommonModule aquí
+  imports: [NgFor, DecimalPipe, RouterLink, NgIf], // Agrega CommonModule aquí
   templateUrl: './country-list.component.html',
 })
 export class CountryListComponent {
