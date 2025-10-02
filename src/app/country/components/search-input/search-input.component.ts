3e5440f0db82a2ee0b4ff1e96d58488ce68292ac
JohanYuri <johanyuri24@gmail.com>
2025-10-02 05:46:16 -0600

    Avance de por region

--- src/app/country/components/search-input/search-input.component.ts
+++ src/app/country/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, signal, effect, OnInit } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -6,7 +6,24 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   imports: [],
   templateUrl: './search-input.component.html',
 })
-export class SearchInputComponent {
+export class SearchInputComponent implements OnInit {
   @Input() placeholder: string = 'buscar';
+  @Input() initialValue: string = '';
   @Output() value = new EventEmitter<string>();
+
+  inputValue = signal<string>('');
+
+  debounceEffect = effect((onCleanup) => {
+    const value = this.inputValue();
+
+    const timeout = setTimeout(() => {
+      this.value.emit(value);
+    }, 500);
+
+    onCleanup(() => clearTimeout(timeout));
+  })
+
+  ngOnInit(): void {
+    this.inputValue.set(this.initialValue);
+  }
 }
