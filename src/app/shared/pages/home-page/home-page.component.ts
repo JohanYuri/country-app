2fdc2c64ac20cd7bad148db7fd8396904a5e3d2c
JohanYuri <johanyuri24@gmail.com>
2025-10-01 19:14:03 -0600

    Se agregaron tables

--- src/app/shared/pages/home-page/home-page.component.ts
+++ src/app/shared/pages/home-page/home-page.component.ts
@@ -7,4 +7,4 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent { }
+export class HomePageComponent {}
