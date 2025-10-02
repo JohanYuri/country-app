5878fe88f70437f993ef649ef35ab4e40afef11a
JohanYuri <johanyuri24@gmail.com>
2025-10-02 05:11:02 -0600

    Se actualizo country.routes.ts

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,9 +1,10 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RESTCountry } from '../interfaces/rest-countries-interfaces';
-import { map, Observable, catchError, throwError, delay } from 'rxjs';
+import { map, Observable, catchError, throwError, delay, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mappers';
+import { Region } from '../interfaces/region.type';
 
 const API_URL = 'https://restcountries.com/v3.1';
 
@@ -11,37 +12,28 @@ const API_URL = 'https://restcountries.com/v3.1';
   providedIn: 'root'
 })
 export class CountryService {
-  
-  private http = inject(HttpClient);
+  private http = inject(HttpClient)
+  private queryCacheCapital = new Map<string, Country[]>();
+  private queryCacheCountry = new Map<string, Country[]>();
+  private queryCacheRegion = new Map<Region, Country[]>();
 
-
-  searchByCapital(query: string): Observable<Country[]> {
-
-    query = query.toLocaleLowerCase();
-
-
-    return this.http
-      .get<RESTCountry[]>(`${API_URL}/capital/${query}`)
-      .pipe(
-        map(resp => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
-        delay(750),
-        catchError(error =>{
-          console.log('Error fetching ', error);
-
-          return throwError(() => new Error(`No se pudo obtener países con el query: ${query}`));
-        })
-      );
-
-  }
-
-
-  searchByCountry( query: string ): Observable<Country[]> {
+  searchByCapital( query: string ): Observable<Country[]> {
     query = query.toLowerCase();
+    // console.log('searchByCountry', query);
+    // return of([]);
 
-    return this.http.get<RESTCountry[]>(`${ API_URL }/name/${ query }`)
+    if ( this.queryCacheCapital.has(query) ) {
+      // console.log('Cache Capital: ', query);
+      return of ( this.queryCacheCapital.get(query)! );
+    }
+
+    console.log('msg a servidor por: ', query);
+
+    return this.http.get<RESTCountry[]>(`${ API_URL }/capital/${ query }`)
     .pipe(
       map(resp => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
-      delay(750),
+      tap((countries) => this.queryCacheCapital.set(query, countries) ),
+      delay(500),
       catchError(error =>{
         console.error('Error fetching countries:', error);
         return throwError(() => new Error(`No se encuentran coincidencias de: "${ query }"`));
@@ -49,15 +41,36 @@ export class CountryService {
     )
   }
 
+  searchByCountry( query: string ): Observable<Country[]> {
+    query = query.toLowerCase();
 
-  searchByCountryByAlphaCode( code: string ){
+    if ( this.queryCacheCountry.has(query) ) {
+      // console.log('Cache Country: ', query);
+      return of ( this.queryCacheCountry.get(query)! );
+    }
+
+    console.log('msg a servidor por: ', query);
+
+    return this.http.get<RESTCountry[]>(`${ API_URL }/name/${ query }`)
+    .pipe(
+      map(resp => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      tap((countries) => this.queryCacheCountry.set(query, countries) ),
+      delay(500),
+      catchError(error =>{
+        console.error('Error fetching countries:', error);
+        return throwError(() => new Error(`No se encuentran coincidencias de: "${ query }"`));
+      })
+    )
+  }
+
+    searchByCountryByAlphaCode( code: string ){
     // code = query.toLowerCase();
 
     return this.http.get<RESTCountry[]>(`${ API_URL }/alpha/${ code }`)
     .pipe(
       map(resp => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
       map (countries => countries.at(0)),
-      delay(750),
+      delay(500),
       catchError(error =>{
         console.error('Error fetching countries:', error);
         return throwError(() => new Error(`No se encuentran un país con el código: "${ code }"`));
@@ -85,8 +98,4 @@ export class CountryService {
       })
     )
   }
-
-
-
-
 }
