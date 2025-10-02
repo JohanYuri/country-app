092727c886c8832590dd77a5440f0afe198007e6
JohanYuri <johanyuri24@gmail.com>
2025-10-01 23:09:26 -0600

    Se muestran las banderas de las capitales

--- src/app/country/components/country-list/country-list.component.ts
+++ src/app/country/components/country-list/country-list.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
+import { RESTCountry } from '../../interfaces/rest-countries-interfaces';
 
 @Component({
   selector: 'country-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // Agrega CommonModule aquí
   templateUrl: './country-list.component.html',
 })
-export class CountryListComponent { }
+export class CountryListComponent {
+  @Input() countries: RESTCountry[] = []; // Define la propiedad como entrada
+
+  trackByCca2(index: number, country: RESTCountry): string {
+    return country.cca2;
+  }
+}
---
+++ src/app/country/services/country.service.ts
@@ -0,0 +1,23 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RESTCountry } from '../interfaces/rest-countries-interfaces';
+
+const API_URL = 'https://restcountries.com/v3.1';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountryService {
+
+  private http = inject(HttpClient);
+
+
+  searchByCapital(query: string) {
+
+    query = query.toLocaleLowerCase();
+
+
+    return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`);
+
+  }
+}
